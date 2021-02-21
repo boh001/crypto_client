cafb73c8aa2ad8e72b2b7013d216de37185abe09
boh001 <boh001g@gmail.com>
2021-02-21 16:49:54 +0900

    chart library

---
+++ components/CandleCharts.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CandleCharts = () => {
+
+  return <div>candle charts</div>
+}
+
+export default CandleCharts
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { NextPage } from "next"
 import socket from "utils/socket"
 
 import Portfolio from 'components/Portfolio'
+import CandleCharts from "components/CandleCharts"
+import '../utils/chart'
 
 type Props = {}
 
@@ -10,6 +12,7 @@ const IndexPage: NextPage<Props> = () => (
     <div>hihi</div>
     <Portfolio />
     <button onClick={() => socket.emit('test', 'test')}>emit</button>
+    <CandleCharts />
   </>
 )
 
